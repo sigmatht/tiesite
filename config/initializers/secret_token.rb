6963c337b5e3ab002cc1f14214ceab150ad3a7f5
sigma <thtsigma@gmail.com>
2012-10-13 23:14:56 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tiesite::Application.config.secret_token = 'c2c003b20d1c096c4abacc883e82263ba480d2f43524caad25f563530f8f3f867007399d8c3dec2b2e0ab78b9b3371e1d81dfc111d277fea44ea140eeb4004c6'
